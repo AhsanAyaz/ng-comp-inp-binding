adb9278c9649ce1510931308261697c803e61491
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2024-02-09 17:23:54 +0100

    refactor: initial state

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,6 +1,5 @@
-import { Component, inject, signal } from '@angular/core';
-import { ProductService } from '../services/product.service';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Product } from '../interfaces/product.interface';
 
 @Component({
@@ -10,25 +9,4 @@ import { Product } from '../interfaces/product.interface';
   templateUrl: './product.component.html',
   styleUrl: './product.component.scss',
 })
-export class ProductComponent {
-  id!: number;
-  product = signal<Product | null>(null);
-  productNormal: Product | null = null;
-  productService = inject(ProductService);
-  activatedRoute = inject(ActivatedRoute);
-
-  ngOnInit() {
-    const id = this.activatedRoute.snapshot.paramMap.get('id');
-    if (!id) {
-      // do something
-      return;
-    }
-    this.id = Number(id);
-    this.productService.getProductById(this.id).subscribe((product) => {
-      this.product.set(product);
-      this.productNormal = product;
-    });
-  }
-
-  goBack() {}
-}
+export class ProductComponent {}
